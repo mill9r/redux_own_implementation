c4e51fe2617ce7aaaac948f4185bef39f9fff26e
mill9r <mill9r@gmail.com>
2018-07-19 17:14:40 +0300

    Добавили Middleware -> update

--- src/redux.js
+++ src/redux.js
@@ -32,46 +32,46 @@ const reducer = (state = 0, action) => {
 // убираем промоис и делаем через так называемый func -> вернем функцию, которая внутри себя принимает dispatch
 // будет аналогичное поведение Promise -> если надо использовать async используют Thunk
 function incrementAsync() {
-    return (dispatch)=>{
-        setTimeout(()=>{
-            dispatch({type:'INCREMENT'})
-        },1000)
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch({type: 'INCREMENT'})
+        }, 1000)
     }
 }
 
 //если action принимает промис, то не даем вернуть значение и вернем только когда промис зарезолвится
-function addPromiseSupport(store, dispatch) {
-    return (action) => {
-        if ('then' in action) {
-            // не даем action попасть в reducer, пока не зарезолвится
-            return action.then(asyncSection => dispatch(asyncSection))
-        }
-
-        return dispatch(action);
+const promiseMiddleware = store => next => action => {
+    //next -> это следующий middleware
+    if ('then' in action) {
+        // не даем action попасть в reducer, пока не зарезолвится
+        // почему не next? посколько прирываем Middleware корректней прогнать next, так мы пропустили middleware
+        //которые были до этого
+        // store.dispatch - Вернуться в начало
+        return action.then(store.dispatch)
     }
-}
 
+    return next(action);
+};
 
-function addLoger(store, dispatch) {
-    return (action) => {
-        console.log('PREVIOUS STATE', store.getState());
-        console.log('ACTION', action);
-        dispatch(action);
-        console.log('CURRENT STATE', store.getState());
+// надо сохранить результат и вернуть его, так рекомендуется возвращать значение из middleware
+const logger = store => next => action => {
+    console.log('PREVIOUS STATE', store.getState());
+    console.log('ACTION', action);
+    const res = next(action);
+    console.log('CURRENT STATE', store.getState());
+    return res;
+};
 
-    }
-}
 
 //добавляем функциональным подход
 // thunk используется вместо асинхронности
-function addThunkSupport(store, dispatch) {
-    return(action)=>{
-        if (typeof action ==='function'){
-            return action(dispatch);
-        }
-        return dispatch(action);
+
+const thunk = store => next => action => {
+    if (typeof action === 'function') {
+        return action(store.dispatch);
     }
-}
+    return next(action);
+};
 
 
 const store = createStore(reducer);
@@ -79,9 +79,21 @@ p = console.log;
 
 
 //прокачиваем dispatch
-store.dispatch = addLoger(store, store.dispatch);
-store.dispatch = addPromiseSupport(store, store.dispatch);
-store.dispatch = addThunkSupport(store, store.dispatch);
+// с каждым dispatchem меняем его , поэтому будем использовать middleware ->называть next -> вместо dispatcher
+// store.dispatch = addLoger(store, store.dispatch);
+// store.dispatch = addPromiseSupport(store, store.dispatch);
+// store.dispatch = addThunkSupport(store, store.dispatch);
+
+// как заменить?
+
+function applyMiddleWare(store,...middlewares) {
+    store.dispatch = middlewares.reduce((result, middleware) => {
+        //result -> next
+        return middleware(store)(result)
+    },store.dispatch);
+}
+
+applyMiddleWare(store, logger, promiseMiddleware, thunk);
 
 store.dispatch(incrementAsync());
 
